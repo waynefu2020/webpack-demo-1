2b5d322bbde4ea5196b5d22b0afd1d1febcd70ad
ddwayne <652507565@qq.com>
2019-12-12 22:23:41 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js'
-  }
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'XDML - 写代码啦',
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
